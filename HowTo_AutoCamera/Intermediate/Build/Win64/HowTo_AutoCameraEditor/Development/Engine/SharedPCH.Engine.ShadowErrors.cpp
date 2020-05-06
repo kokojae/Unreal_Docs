af613a57ca248936756d2c03926df5b023ae8d5d
kokojae <54296776+kokojae@users.noreply.github.com>
2020-05-06 11:48:06 +0900

    게임 조종 카메라 프로젝트 생성
    게임 조종 카메라 실습을 위한 프로젝트 생성함

---
+++ HowTo_AutoCamera/Intermediate/Build/Win64/HowTo_AutoCameraEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "D:/WorkOutput/Unreal_Docs/HowTo_AutoCamera/Intermediate/Build/Win64/HowTo_AutoCameraEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ HowTo_AutoCamera/Intermediate/Build/Win64/UE4Editor/Inc/HowTo_AutoCamera/HowTo_AutoCameraGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HOWTO_AUTOCAMERA_HowTo_AutoCameraGameModeBase_generated_h
+#error "HowTo_AutoCameraGameModeBase.generated.h already included, missing '#pragma once' in HowTo_AutoCameraGameModeBase.h"
+#endif
+#define HOWTO_AUTOCAMERA_HowTo_AutoCameraGameModeBase_generated_h
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_SPARSE_DATA
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_RPC_WRAPPERS
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAHowTo_AutoCameraGameModeBase(); \
+	friend struct Z_Construct_UClass_AHowTo_AutoCameraGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHowTo_AutoCameraGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HowTo_AutoCamera"), NO_API) \
+	DECLARE_SERIALIZER(AHowTo_AutoCameraGameModeBase)
+
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAHowTo_AutoCameraGameModeBase(); \
+	friend struct Z_Construct_UClass_AHowTo_AutoCameraGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHowTo_AutoCameraGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HowTo_AutoCamera"), NO_API) \
+	DECLARE_SERIALIZER(AHowTo_AutoCameraGameModeBase)
+
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHowTo_AutoCameraGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHowTo_AutoCameraGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHowTo_AutoCameraGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHowTo_AutoCameraGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHowTo_AutoCameraGameModeBase(AHowTo_AutoCameraGameModeBase&&); \
+	NO_API AHowTo_AutoCameraGameModeBase(const AHowTo_AutoCameraGameModeBase&); \
+public:
+
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHowTo_AutoCameraGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHowTo_AutoCameraGameModeBase(AHowTo_AutoCameraGameModeBase&&); \
+	NO_API AHowTo_AutoCameraGameModeBase(const AHowTo_AutoCameraGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHowTo_AutoCameraGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHowTo_AutoCameraGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHowTo_AutoCameraGameModeBase)
+
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_12_PROLOG
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_SPARSE_DATA \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_RPC_WRAPPERS \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_INCLASS \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_SPARSE_DATA \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HOWTO_AUTOCAMERA_API UClass* StaticClass<class AHowTo_AutoCameraGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HowTo_AutoCamera_Source_HowTo_AutoCamera_HowTo_AutoCameraGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
